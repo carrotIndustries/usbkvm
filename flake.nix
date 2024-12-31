ba0f458aac2bc82a9eabd4a0570b96beb072c6cf
clerie <git@clerie.de>
2024-12-31 13:26:21 +0100

    Expose mslib as nix flake

--- flake.nix
+++ flake.nix
@@ -10,6 +10,24 @@
         inherit system;
       };
     in rec {
+      mslib = pkgs.buildGoModule {
+        pname = "mslib";
+        version = "0.0.1";
+
+        src = ./. + "/app/ms-tools";
+
+        buildInputs = with pkgs; [
+          hidapi
+        ];
+
+        buildPhase = ''
+          mkdir -p $out
+          go build -C lib -o $out -buildmode=c-archive mslib.go
+        '';
+
+        vendorHash = "sha256-imHpsos7RDpATSZFWRxug67F7VgjRTT1SkLt7cWk6tU=";
+      };
+
       usbkvm-udev = pkgs.runCommand "usbkvm-udev" {} ''
         mkdir -p $out/lib/udev/rules.d
         cp ${./app}/70-usbkvm.rules $out/lib/udev/rules.d
